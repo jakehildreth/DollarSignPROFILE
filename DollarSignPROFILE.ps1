27e28a5d1417cde3813fa64cf0c0431d4c3f4962
Jake Hildreth <93942157+TrimarcJake@users.noreply.github.com>
2024-08-13 19:01:30 -0400

    Initial commit

---
+++ DollarSignPROFILE.ps1
@@ -0,0 +1,47 @@
+function New-Function {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string]$FunctionName,
+        [Parameter(Mandatory, Position = 1)]
+        [string]$Path
+    )
+
+    #requires -Version 5
+
+    $Path = Join-Path -Path $Path -ChildPath "$($FunctionName).ps1"
+    $Framework = @"
+function $FunctionName {
+    <#
+        .SYNOPSIS
+
+        .DESCRIPTION
+
+        .PARAMETER
+
+        .INPUTS
+
+        .OUTPUTS
+
+        .EXAMPLE
+
+        .LINK
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    #requires -Version 5
+
+    begin {
+    }
+
+    process {
+    }
+
+    end {
+    }
+}
+"@
+    $Framework | Out-File -Path $Path
+}
