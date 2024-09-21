e56e398f5b1b7fce219f30c64fb0b8ab5b0b9d6b
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2024-09-21 08:29:21 -0500

    thing

---
+++ LocksmithLogo.ps1
@@ -0,0 +1,83 @@
+Write-Host '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '..........' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '................' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '......' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '......' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.....' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.....' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.....' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.....' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.....' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.....' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '..........................' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.........' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '...............' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '...........' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.............' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.............' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '...........' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '............' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '...........' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.........' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '..............' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '.......' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '....'  -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '........' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta -NoNewline
+Write-Host '..........................' -BackgroundColor Red -ForegroundColor Red -NoNewline
+Write-Host '%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
+
+Write-Host '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%' -BackgroundColor DarkMagenta -ForegroundColor DarkMagenta
