0cdaeb683e142929970cb355ea6a05c039ce3e3d
Jake Hildreth <jakehildreth@gmail.com>
2025-12-19 07:16:18 -0500

    Added PS Major Version into prompt so I dont need to run $PSVersionTable so often

--- DollarSignPROFILE.ps1
+++ DollarSignPROFILE.ps1
@@ -72,18 +72,11 @@ function prompt {
     $CurrentLocation = $executionContext.SessionState.Path.CurrentLocation
     [string]$GitBranch = git branch --show-current
     if ($?) {
-        # [int]$Ahead = (git rev-list --left-right --count $GitBranch).Split()[0]
-        # [int]$Behind = (git rev-list --left-right --count $GitBranch).Split()[1]
-        # Write-Host -NoNewLine "$CurrentLocation [$GitBranch "
-        # Write-Host "+$Ahead " -NoNewLine -ForegroundColor Green
-        # Write-Host "-$Behind" -NoNewLine -ForegroundColor Red
-        # Write-Host "]"
-        Write-Host "$($CurrentLocation.ToString() -ireplace [regex]::escape($HOME),'~') [$GitBranch]" 
-        # Write-Host "$CurrentLocation [$GitBranch]"
+        Write-Host "$($CurrentLocation.ToString() -ireplace [regex]::escape($HOME),'~') [$GitBranch]"
     } else {
         Write-Host "$($CurrentLocation.ToString() -ireplace [regex]::escape($HOME),'~')" 
     }
-    "PS$('>' * ($nestedPromptLevel + 1)) "
+    "PS$($PSVersionTable.PSVersion.Major)$('>' * ($nestedPromptLevel + 1)) "
 }
 
 $PSDefaultParameterValues = @{
