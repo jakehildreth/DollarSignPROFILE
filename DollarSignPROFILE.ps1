db0b718a15185e150fc31fe83a266524c720c086
Jake Hildreth <93942157+jakehildreth@users.noreply.github.com>
2026-04-16 20:48:52 -0400

    Update comment in DollarSignPROFILE.ps1

--- DollarSignPROFILE.ps1
+++ DollarSignPROFILE.ps1
@@ -1,4 +1,4 @@
-# Sometimes, you have to make a change.
+# Sometimes, you change back.
 #region Self-Update
 try {
     $selfUpdateUrl = 'https://raw.githubusercontent.com/jakehildreth/DollarSignPROFILE/refs/heads/main/DollarSignPROFILE.ps1'
